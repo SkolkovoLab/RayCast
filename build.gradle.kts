8a6443862f7fededa1521594a45fe5674a80c897
MelonHell <MelonHell@ya.ru>
2024-05-06 17:56:46 +0300

    minestom compileOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.minestom:minestom-snapshots:fed512eaf6")
+    compileOnly("net.minestom:minestom-snapshots:fed512eaf6")
     implementation("org.apache.commons:commons-geometry-euclidean:1.0")
 
     compileOnly("org.jetbrains:annotations:24.1.0")
